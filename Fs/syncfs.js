4117c253d9723d6fb5d985a31a61cd0efc8f0294
Reshoo-18 <reshooranjan7@gmail.com>
2025-08-19 22:52:02 +0530

    v12

---
+++ Fs/syncfs.js
@@ -0,0 +1,27 @@
+const fs = require("fs"); 
+const path = require("path");
+const fileName="text.txt"
+const filePath= path.join(__dirname,fileName);
+const writeFile = fs.writeFileSync(filePath,"all the data here watch  it ,updated","utf-8")
+console.log(writeFile)
+
+
+
+
+
+const readFile= fs.readFileSync(filePath,"utf-8");
+console.log(readFile);
+
+
+
+
+
+
+const appendFile =fs.appendFileSync(filePath,
+"\nthe new data is here ","utf-8");
+
+console.log(appendFile)
+
+
+// const unlinkFile =fs.unlinkSync(filePath,"utf-8")
+// console.log(unlinkFile)
