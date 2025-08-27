c822483e605876c9e39019e3635c2ca1d0fb692d
Reshoo-18 <reshooranjan7@gmail.com>
2025-08-27 23:50:25 +0530

    vid 8

--- PIYUSHGARG/HTTP SERVER/index.js
+++ PIYUSHGARG/HTTP SERVER/index.js
@@ -1,13 +1,21 @@
 const http =require('http');
 const fs =require('fs');
+const url=require('url');
 const myServer=http.createServer((req,res)=>{
     const log=`${Date.now()} ${req.url}:New require received\n`;
+    const myUrl= url.parse(req.url,true);
+    console.log(myUrl)
     fs.appendFile('log.txt',log,(err,data)=>{
-        switch(req.url){
+        if(req.url==="/favicon.ico") return res.end();
+        switch(myUrl.pathname){
             case"/":res.end("homePage");
             break
-            case"/about":res.end("hello i am reshoo")
-            default:res.end("404")
+            case"/about":
+            const username =myUrl.query.myname; 
+            res.end(`hi,${username}`)
+            break
+            default:
+                res.end("404 not found")
         }
     })
 })
