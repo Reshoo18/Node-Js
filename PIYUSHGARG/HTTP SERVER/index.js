109853972e1e13f1ca2c7f7e14f6a882132b438d
Reshoo-18 <reshooranjan7@gmail.com>
2025-08-28 23:14:09 +0530

    vid 9,10

--- PIYUSHGARG/HTTP SERVER/index.js
+++ PIYUSHGARG/HTTP SERVER/index.js
@@ -1,23 +1,19 @@
-const http =require('http');
-const fs =require('fs');
-const url=require('url');
-const myServer=http.createServer((req,res)=>{
-    const log=`${Date.now()} ${req.url}:New require received\n`;
-    const myUrl= url.parse(req.url,true);
-    console.log(myUrl)
-    fs.appendFile('log.txt',log,(err,data)=>{
-        if(req.url==="/favicon.ico") return res.end();
-        switch(myUrl.pathname){
-            case"/":res.end("homePage");
-            break
-            case"/about":
-            const username =myUrl.query.myname; 
-            res.end(`hi,${username}`)
-            break
-            default:
-                res.end("404 not found")
-        }
-    })
+
+const express=require('express');
+
+const app=express();
+app.get('/',(req,res)=>{
+    return res.send('hello from home page')
 })
 
-myServer.listen(8000,()=>{console.log("server started")})
+app.get('/about',(req,res)=>{
+    return res.send('the page is for about' +"hey"+req.query.name+"age"+req.query.age)
+})
+
+
+function myHandler(req,res){
+
+}
+
+
+app.listen(8000,()=>{console.log("server started")})
