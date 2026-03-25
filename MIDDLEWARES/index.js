c64e54db2986941f35dc93b9d155b097043ba14c
Reshoo-18 <reshooranjan7@gmail.com>
2026-03-25 23:34:56 +0530

    MIDDLEWARES

---
+++ MIDDLEWARES/index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app=express();
+const PORT=3000;
+
+app.use(express.json())
+
+const loginMiddleWare=function (req,res,next){
+    console.log("LOGIN KAR RHA HU")
+    next();
+}
+app.use(loginMiddleWare)
+const authMiddleWare=function(req,res,next){
+    console.log("AUTH KAR RHA HU")
+    next();
+}
+app.use(authMiddleWare)
+
+const validationMiddleWare=function(req,res,next){
+    console.log("CHECK KAR RHA HU")
+    next();
+}
+app.use(validationMiddleWare)
+app.get('/',(req, res)=>{
+    console.log("mai router ka middleware hu")
+    //console.log(req.body)
+    res.send("hello world");
+})
+
+app.listen(PORT,()=>{
+    console.log(`SERVER AT LISTENING AT PORT ${PORT}`)
+} )
