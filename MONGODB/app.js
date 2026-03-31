9c6773ebaf2804f12867b883729cab7a0b856a3b
Reshoo-18 <reshooranjan7@gmail.com>
2026-03-31 22:49:03 +0530

    CRUD OPERATION DONE

--- MONGODB/app.js
+++ MONGODB/app.js
@@ -8,7 +8,8 @@ const PORT=3000;
 app.get('/',(req,res)=>{
     res.send("this is the home page")
 })
-
+//CRUD OPERATION
+//CREATE
 app.get('/create',async (req,res)=>{
    let createdUser=await userModel.create({
     name:"reshoo",
@@ -16,6 +17,23 @@ app.get('/create',async (req,res)=>{
     email:"reshoo123@gmail.com"
    })
    res.send(createdUser)
+})
+
+//Update
+app.get('/update',async(req,res)=>{
+
+    const updatedUser=  await userModel.findOneAndUpdate({username:"reshooranja"},{name:"reshoo ranjan"},{new:true})
+    res.send(updatedUser)
+})
+
+app.get('/read', async(req,res)=>{
+    const readUser= await userModel.find();
+    res.send(readUser)
+})
+
+app.get('/delete', async(req,res)=>{
+    const deleteUser= await userModel.findOneAndDelete({username:"reshooranja"});
+    res.send(deleteUser)
 })
 
 app.listen(PORT,()=>{
